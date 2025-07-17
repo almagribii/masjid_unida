d816cd41f76358837839d98be0fb1b1a2ee51fc7
brcadal <brucadal@gmail.com>
2025-07-17 16:37:48 +0700

    bantuan menggunakan form

--- app/src/main/java/com/example/masjid_annur/activity/BantuanActivity.kt
+++ app/src/main/java/com/example/masjid_annur/activity/BantuanActivity.kt
@@ -1,6 +1,8 @@
 package com.example.masjid_annur.activity
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +15,17 @@ class BantuanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bantuan)
+        setContentView(R.layout.activity_bantuan2)
         btnBack = findViewById(R.id.btnBack)
 
         btnBack.setOnClickListener {
             finish()
         }
+
+        val webView : WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSe5Xw9Kv9l_-Wgd4_9x5URdnYE1ccPbKEZ1ubENdnGXP-Aw9w/viewform?usp=dialog")
 
 //        btnBack.setOnClickListener() {
 //            onBackPressed()
